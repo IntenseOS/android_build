66a7f576f348cad2824072eac0404bec7ec7b1af
vibhu0009 <cvibhu0009@gmail.com>
2015-10-23 11:17:35 +0200

    make intense

--- envsetup.sh
+++ envsetup.sh
@@ -534,7 +534,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka bacon
+        mka intense
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
@@ -2110,7 +2110,7 @@ function cmka() {
     if [ ! -z "$1" ]; then
         for i in "$@"; do
             case $i in
-                bacon|otapackage|systemimage)
+                intense|otapackage|systemimage)
                     mka installclean
                     mka $i
                     ;;
